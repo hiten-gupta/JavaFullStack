53492fd69c9c471babb6d7775351e82260310b91
Hiten <guptahiten534@protonmail.com>
2026-06-11 13:01:09 +0530

    DOM

--- FrontEnd codes/JavaScript Learn/dom.js
+++ FrontEnd codes/JavaScript Learn/dom.js
@@ -5,3 +5,20 @@ function subscribeButton(){
     }else buttonElement.innerText = 'Subscribe';
     
 }
+function calculate(){
+    const inputElement = document.getElementById('fname');
+    let orderTotal = Number(inputElement.value);
+    let finalCost = 0;
+    if(isNaN(orderTotal) || orderTotal <=0){
+        document.getElementById('total-cost').innerText = "Enter a valid amount";
+        return;
+    }
+    if(orderTotal < 500) finalCost = orderTotal +150;
+    else finalCost = orderTotal;
+    document.getElementById('total-cost').innerText = `Total Amount : RS. ${finalCost}`;
+}
+function helperEnter(){
+    if(event.key === 'Enter'){
+        calculate();
+    }
+}
